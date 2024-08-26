a61f11d8336afc71f27c85d97a9969bfbf3a9db1
Dror <drordvash4@gmail.com>
2024-08-26 16:39:36 +0200

    added welcome screen

--- src/navigation/unAuthenticated/tabs/unauthenticated-tabs.tsx
+++ src/navigation/unAuthenticated/tabs/unauthenticated-tabs.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { AuthNavigator } from "../auth/auth-navigator";
-// import { WelcomeScreen } from "../../../screens/welcome/welcome-screen";
+import { WelcomeScreen } from "../../../screens/welcome/welcome-screen";
 
 const Tab = createBottomTabNavigator();
 
 export const UnAuthenticatedTabs = () => {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      {/* <Tab.Screen
+      <Tab.Screen
         name="Welcome"
         component={WelcomeScreen}
         options={{
@@ -17,7 +17,7 @@ export const UnAuthenticatedTabs = () => {
             <Icon name="home" color={color} size={size} />
           ),
         }}
-      /> */}
+      />
       <Tab.Screen
         name="User"
         component={AuthNavigator}
---
+++ src/screens/welcome/welcome-screen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const WelcomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome Screen</Text>
+    </View>
+  );
+};
