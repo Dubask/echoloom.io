79b36618f7c8d01973d7d8a61cd9495dff9e0096
Dror <drordvash4@gmail.com>
2024-08-27 14:00:53 +0200

    removed safe area from app

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootNavigator } from "./src/navigation/root-navigator";
 import "react-native-gesture-handler";
@@ -13,14 +13,9 @@ const App = () => {
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        {/* <SafeAreaView
-          style={{ flex: 1, backgroundColor: "transparent" }}
-          edges={["left", "right", "bottom"]}
-        > */}
         <NavigationContainer>
           <RootNavigator />
         </NavigationContainer>
-        {/* </SafeAreaView> */}
       </GestureHandlerRootView>
     </SafeAreaProvider>
   );
