7924a3ce00af4126561c4e6f932653229670a659
Dror <drordvash4@gmail.com>
2024-08-27 13:06:42 +0200

    added controllers + refactor and component saparation

--- src/screens/welcome/welcome-screen.tsx
+++ src/screens/welcome/welcome-screen.tsx
@@ -2,46 +2,57 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useState } from "react";
 import { View } from "react-native-ui-lib";
 import { UnAuthenticatedStackParamList } from "../../navigation/unAuthenticated/tabs/unauthenticated-tabs";
-import { ImageGallery } from "./welcome-gallery/image-gallery";
-import { VideoGallery } from "./welcome-gallery/video-gallery";
+import { ImageGallery } from "./welcome-gallery/image-gallery/image-gallery";
+import { VideoGallery } from "./welcome-gallery/video-gallery/video-gallery";
+import { GALLERY_ASSETS } from "./welcome-gallery/const";
+import { GalleryControllers } from "./welcome-gallery/gallery-controllers/gallery-controllers";
 
-type WelcomeScreenProps = NativeStackScreenProps<
+type NavigationProps = NativeStackScreenProps<
   UnAuthenticatedStackParamList,
   "Welcome"
 >;
 
-export const WelcomeScreen = ({ navigation }: WelcomeScreenProps) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+export const WelcomeScreen = ({ navigation }: NavigationProps) => {
+  const [currnetGalleryIndex, setCurrentGalleryIndex] = useState(0);
 
-  const handleNext = () => {
-    if (currentIndex < images.length) {
-      setCurrentIndex(currentIndex + 1);
-    } else {
+  const isLastItem = currnetGalleryIndex === GALLERY_ASSETS.length - 1;
+  const isFirstItem = currnetGalleryIndex === 0;
+  const currentGalleryItem = GALLERY_ASSETS[currnetGalleryIndex];
+  const isVideo = currentGalleryItem.type === "video";
+  const isImage = currentGalleryItem.type === "image";
+
+  const onNext = () => {
+    if (isLastItem) {
       navigation.navigate("Welcome");
+    } else {
+      setCurrentGalleryIndex(currnetGalleryIndex + 1);
     }
   };
 
-  const images = [
-    require("../../../assets/demo-img1.jpg"),
-    require("../../../assets/demo-img2.jpg"),
-  ];
-
-  const handleSkip = () => {
-    // go to register
-    // navigation.navigate("HomeScreen");
-  };
+  const handleBack = () => setCurrentGalleryIndex(currnetGalleryIndex - 1);
 
   return (
-    <View flex bg-black>
-      {currentIndex < images.length ? (
-        <ImageGallery
-          images={images}
-          currentIndex={currentIndex}
-          handleNext={handleNext}
-        />
-      ) : (
-        <VideoGallery handleNext={handleNext} />
-      )}
+    <View flex>
+      <GalleryControllers
+        isLastItem={isLastItem}
+        isFirstItem={isFirstItem}
+        handleBack={handleBack}
+      />
+      <View flex>
+        {isImage && (
+          <ImageGallery
+            buttonText={isLastItem ? "Let's Try" : "Next"}
+            currentGalleryItem={currentGalleryItem.asset}
+            onPress={onNext}
+          />
+        )}
+        {isVideo && (
+          <VideoGallery
+            handleNext={onNext}
+            currentGalleryItem={currentGalleryItem.asset}
+          />
+        )}
+      </View>
     </View>
   );
 };
