108dbaa5cf414e920779b84ce8cfc11996fad2d1
Dror <drordvash4@gmail.com>
2024-08-30 17:40:29 +0200

    done apple +

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -4,11 +4,13 @@ import auth from "@react-native-firebase/auth";
 import firestore from "@react-native-firebase/firestore";
 
 const firebaseConfig = Constants.expoConfig?.extra?.firebase;
+console.log("firebaseConfig:", firebaseConfig);
 
 if (!fb.apps.length) {
   fb.initializeApp(firebaseConfig);
   console.log("Firebase initialized successfully");
 } else {
+  fb.app();
   console.log("Firebase app already initialized.");
 }
 
